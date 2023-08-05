d8be2849d92f838db38ca2440915b697d4db12e8
Butnaru Gheorghita <butnaru.gheorghita@yahoo.com>
2023-08-05 15:26:11 +0300

    Added role claims in the Authentication object

--- web-player-server/src/main/java/dev/gb/webplayerserver/configs/security/WebSecurityConfig.java
+++ web-player-server/src/main/java/dev/gb/webplayerserver/configs/security/WebSecurityConfig.java
@@ -4,15 +4,21 @@ package dev.gb.webplayerserver.configs.security;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class WebSecurityConfig {
 
+@Configuration
+@EnableMethodSecurity
+public class WebSecurityConfig {
     @Value("${jwt.set-uri:http://localhost:8080/oauth2/jwks}")
     private String jwtSetUri;
 
+    public static final String JWT_AUTHORITIES_CLAIM_NAME = "roles";
+    public static final String JWT_AUTHORITY_PREFIX = "";
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,5 +30,16 @@ public class WebSecurityConfig {
                         .jwt(jwt -> jwt.jwkSetUri(jwtSetUri))
                 );
         return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName(JWT_AUTHORITIES_CLAIM_NAME);
+        grantedAuthoritiesConverter.setAuthorityPrefix(JWT_AUTHORITY_PREFIX);
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 }
